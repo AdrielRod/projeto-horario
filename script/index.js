d6ff1abf6b4a18d9df9b65a08ac988cc6ec19d1c
Adriel Rodrigues <117696260+AdrielRod@users.noreply.github.com>
2023-01-04 19:14:03 -0300

    projeto horario finalizado

--- script/index.js
+++ script/index.js
@@ -1,9 +1,31 @@
-var data = new Date()
-var hora = data.getHours()
-var palavra = document.querySelector("p#horaAtual")
-var imagem = document.querySelector("img#imagem")
-palavra.innerText = (`São exatamente ${hora} horas.`)
+function carregar(){
+    var palavra = document.querySelector("p#horaAtual")
+    //Pegando os dados do parágrafo
 
-if (hora >= 13){
-    imagem.innerText = "tarde"
+    var imagem = document.getElementById("imagem")
+
+    var data = new Date()
+    //Instanciando o objeto Date
+
+    var hora = data.getHours()
+    //Adquirindo o valor das horas
+
+    var foto = document.getElementById("foto")
+    //Pegando os dados da foto
+
+    palavra.innerText = (`São exatamente ${hora} horas.`)
+
+
+    if (hora >= 0 && hora < 12){
+        document.getElementById("imagem").src = "imagens/manha.jpg"
+        
+    }
+    else if (hora >= 13 && hora < 17){
+        document.getElementById("imagem").src = "imagens/tarde.jpg"
+        document.body.style.backgroundColor = "#c2bb5d"
+    }
+    else{
+        document.getElementById("imagem").src = "imagens/noite.jpg"
+        document.body.style.backgroundColor = "#4e4e4b"
+    }
 }
